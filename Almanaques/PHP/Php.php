900d71a24639a3d39e78e8b60ce3160e13d812ed
Julio Alvarado <julyuzc@gmail.com>
2021-10-02 15:26:55 -0500

    Update Php.php
    Otro código para subir una imagen

--- Almanaques/PHP/Php.php
+++ Almanaques/PHP/Php.php
@@ -15,4 +15,20 @@
       $abrir = fopen($ruta.'/'.$_POST['imgNom'],'w');
       fwrite($abrir, base64_decode($_POST['imgBase']));
     }
+
+    // Subir imagen
+    $upload = 'err';
+    if (!empty($_FILES['file'])) {
+          $targetDir = "uploads/";
+          $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg', 'gif');
+          $fileName = basename($_FILES['file']['name']);
+          $targetFilePath = $targetDir . $fileName;
+          $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+          if (in_array($fileType, $allowTypes)) {
+                if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFilePath)) {
+                      $upload = 'ok';
+                }
+          }
+    }
+    echo $upload; 
 ?>
